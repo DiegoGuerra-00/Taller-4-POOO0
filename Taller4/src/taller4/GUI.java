0dfd3e138b33718987e2b8d39e6c58fc696776b1
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-12-03 22:20:16 -0300

    ACTUALIZACION DE LA GUI, JUNTO CON COMENTARIOS

--- Taller4/src/taller4/GUI.java
+++ Taller4/src/taller4/GUI.java
@@ -1,5 +1,8 @@
 package taller4;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,9 +14,14 @@ public class GUI extends JFrame{
 	JButton botonCoordinador;
 	JButton botonEstudiante;
 	
-	JLabel textoAmdmin;
-	JLabel textoCoordinador;
-	JLabel textoEstudiante;
+	JButton gestionUsuarios;
+	JButton gestionCertificacion;
+	JButton panelMetricasYanalisis;
+	JButton gestionEstudiantes;
+	JButton perfilEstudiante;
+	JButton mallaCurricular;
+	JButton inscripcionCertificaciones;
+	JButton seguimientoProceso;
 	
 	public GUI() {
 		pantalla = new JFrame();
@@ -40,10 +48,91 @@ public class GUI extends JFrame{
 		pantalla.add(botonAdmin);
 		pantalla.add(botonCoordinador);
 		pantalla.add(botonEstudiante);
+		
+		mostrarGestionUsuario(botonAdmin);
 
 	
 		
 	}
+	private void limpiarPantallaTOTAL() {
+		pantalla.getContentPane().removeAll();
+		pantalla.repaint();
+		pantalla.revalidate();
+		
+		
+		
+		pantalla.add(botonAdmin);
+		pantalla.add(botonCoordinador);
+		pantalla.add(botonEstudiante);
+		
+		pantalla.repaint();
+		
+	}
+	private void mostrarGestionUsuario(JButton boton) {
+		boton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				limpiarPantalla();
+				gestionUsuarios = new JButton();
+				gestionUsuarios.setBounds(1,100,200,30);
+				gestionUsuarios.setText("Gestión de Usuarios");
+				pantalla.add(gestionUsuarios);
+				
+				gestionUsuarios.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						limpiarPantalla();
+						
+						JButton crearCuentasEstudianteCoorinador =  new JButton();
+						crearCuentasEstudianteCoorinador.setText("Crear cuentas de estudiante o coordinador");
+						JButton modificarcuentasEstudianteCoordinador =  new JButton();
+						modificarcuentasEstudianteCoordinador.setText("Modificar cuentas de estudiante o coordinador");
+						JButton eliminarCuentasEstudianteCoordinador =  new JButton();
+						eliminarCuentasEstudianteCoordinador.setText("Eliminar cuentas de estudiante o coordinador");
+						JButton restablecerContraseñas =  new JButton();
+						restablecerContraseñas.setText("Restablecer contraseñas");
+
+						crearCuentasEstudianteCoorinador.setBounds(1,50,200,30);
+						modificarcuentasEstudianteCoordinador.setBounds(1,100,200,30);
+						eliminarCuentasEstudianteCoordinador.setBounds(1,200,200,30);
+						restablecerContraseñas.setBounds(1,250,200,30);
+						
+					
+						pantalla.add(crearCuentasEstudianteCoorinador);
+						pantalla.add(modificarcuentasEstudianteCoordinador);
+						pantalla.add(eliminarCuentasEstudianteCoordinador);
+						pantalla.add(restablecerContraseñas);
+						volveR();
+						
+
+						
+					}
+				});
+			}
+		});
+		
+		
+		
+		
+	}
+	private void limpiarPantalla() {
+		pantalla.getContentPane().removeAll();
+		pantalla.repaint();
+		pantalla.revalidate();
+	}
+	private void volveR() {
+		JButton volver = new JButton("Volver");
+        volver.setBounds(10, 300, 100, 30);
+        
+       volver.addActionListener(ev -> {
+    	   limpiarPantallaTOTAL();
+       });
+       pantalla.add(volver);
+		
+	}
+	
 	
 	
 
